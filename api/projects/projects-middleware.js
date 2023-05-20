5f30f644f00fd5beeccb1831b23cfbe589d7b8b7
cosmopoliitans <elfkyn52@gmail.com>
2023-05-20 17:37:32 +0300

    Proje tamamlandı

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,34 @@
 // projects ara yazılımları buraya
+const pm = require("./projects-model");
+
+async function validateProjectId(req, res, next) {
+  try {
+    const isExistProject = await pm.get(req.params.id);
+    if (!isExistProject) {
+      res.status(404).json({ message: "Proje bulunamadı." });
+    } else {
+      req.currentProject = isExistProject;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+function validateProjectPayload(req, res, next) {
+  try {
+    const { name, description } = req.body;
+    if (!name || !description) {
+      res.status(400).json({ message: "Girilen bilgileri kontrol ediniz." });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = {
+  validateProjectId,
+  validateProjectPayload,
+};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,74 @@
 // "project" routerını buraya yazın!
+const router = require("express").Router();
+const pm = require("./projects-model");
+const mw = require("./projects-middleware");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const allProjects = await pm.get();
+    res.json(allProjects);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", mw.validateProjectId, (req, res, next) => {
+  try {
+    res.json(req.currentProject);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", mw.validateProjectPayload, async (req, res, next) => {
+  try {
+    let model = {
+      name: req.body.name,
+      description: req.body.description,
+      completed: req.body.completed,
+    };
+    const inserted = await pm.insert(model);
+    res.status(201).json(inserted);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put(
+  "/:id",
+  mw.validateProjectId,
+  mw.validateProjectPayload,
+  async (req, res, next) => {
+    try {
+      let model = {
+        name: req.body.name,
+        description: req.body.description,
+        completed: req.body.completed,
+      };
+      const update = await pm.update(req.params.id, model);
+      res.json(update);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete("/:id", mw.validateProjectId, async (req, res, next) => {
+  try {
+    await pm.remove(req.params.id);
+    res.json({ message: "Silme işlemi tamamlandı." });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id/actions", mw.validateProjectId, async (req, res, next) => {
+  try {
+    const projectActions = await pm.getProjectActions(req.params.id);
+    res.json(projectActions);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
